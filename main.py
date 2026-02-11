86f3a4b99fae1b653721428cc98703c31fa0a033
Piotr <latek89@gmail.com>
2026-02-11 08:47:34 +0100

    Progress bar / frequency of intervals

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 # MouseMover by Piotr Latka
 # logika + konfiguracja
 
-
 import pyautogui as pag
 import random
 import time
 import threading
+from tkinter import ttk
+
 
 pag.FAILSAFE = True
 
@@ -17,14 +18,18 @@ class MouseMover:
         self.status_callback = status_callback
 
         self.afk_limit = 180
+        self.move_interval = 5
         self.sleep_time = 1
+
         self.screen_width, self.screen_height = pag.size()
 
         self.afk_seconds = 0
         self.total_seconds = 0
+        self.active_mode = False
 
-    def update_settings(self, afk_limit, width, height):
+    def update_settings(self, afk_limit, move_interval, width, height):
         self.afk_limit = afk_limit
+        self.move_interval = move_interval
         self.screen_width = width
         self.screen_height = height
 
@@ -33,6 +38,7 @@ class MouseMover:
             self.running = True
             self.afk_seconds = 0
             self.total_seconds = 0
+            self.active_mode = False
             self.thread = threading.Thread(target=self.run, daemon=True)
             self.thread.start()
 
@@ -46,21 +52,32 @@ class MouseMover:
             current_pos = pag.position()
             self.total_seconds += 1
 
-            if current_pos == curr_coords:
-                self.afk_seconds += 1
-            else:
+            if current_pos != curr_coords:
                 self.afk_seconds = 0
+                self.active_mode = False
                 curr_coords = current_pos
-
-            if self.status_callback:
-                self.status_callback(self.afk_seconds, self.total_seconds)
+            else:
+                self.afk_seconds += 1
 
             if self.afk_seconds >= self.afk_limit:
+                self.active_mode = True
+
+            # Aktualizacja GUI
+            if self.status_callback:
+                self.status_callback(
+                    self.afk_seconds,
+                    self.total_seconds,
+                    self.active_mode,
+                    self.afk_limit
+                )
+
+            if self.active_mode:
                 x = random.randint(0, self.screen_width - 1)
                 y = random.randint(0, self.screen_height - 1)
 
                 pag.moveTo(x, y, duration=0.5)
                 curr_coords = pag.position()
-                self.afk_seconds = 0
 
-            time.sleep(self.sleep_time)
+                time.sleep(self.move_interval)
+            else:
+                time.sleep(self.sleep_time)
